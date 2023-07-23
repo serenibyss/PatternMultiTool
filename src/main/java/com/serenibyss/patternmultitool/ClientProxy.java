35889e3e4f4e927c6d056fa1d930bd5fd54375e8
serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-07-23 16:31:10 -0500

    initial commit

---
+++ src/main/java/com/serenibyss/patternmultitool/ClientProxy.java
@@ -0,0 +1,4 @@
+package com.serenibyss.patternmultitool;
+
+public class ClientProxy extends CommonProxy {
+}
---
+++ src/main/java/com/serenibyss/patternmultitool/PatternMultiTool.java
@@ -0,0 +1,41 @@
+package com.serenibyss.patternmultitool;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")
+public class PatternMultiTool {
+
+    public static final Logger LOG = LogManager.getLogger(Tags.MODID);
+
+    @SidedProxy(clientSide = "com.serenibyss.patternmultitool.ClientProxy", serverSide = "com.serenibyss.patternmultitool.CommonProxy")
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+
+    @EventHandler
+    public void serverStarting(FMLServerStartingEvent event) {
+        proxy.serverStarting(event);
+    }
+}
